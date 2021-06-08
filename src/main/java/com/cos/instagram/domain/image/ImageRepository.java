1ae50a636c1e83d495e5c9f7bb9e0a4defd731d6
cocoblue <cocoblue@kakao.com>
2021-06-08 15:29:06 +0900

    - 피드 작동 불가문제 해결

--- src/main/java/com/cos/instagram/domain/image/ImageRepository.java
+++ src/main/java/com/cos/instagram/domain/image/ImageRepository.java
@@ -10,7 +10,7 @@ public interface ImageRepository extends JpaRepository<Image, Integer>{
 	List<Image> findByUserId(int userId);
 	
 	// 내가 팔로우 하지 않은 사람들의 이미지들(최대 20개)
-	@Query(value = "select * from Image where userId in (select id from User where id != ?1 and id not in (select toUserId from Follow where fromUserId = ?1)) limit 20 ORDER BY createDate DESC", nativeQuery = true)
+	@Query(value = "select * from Image where userId in (select id from User where id != ?1 and id not in (select toUserId from Follow where fromUserId = ?1))  ORDER BY createDate DESC limit 20", nativeQuery = true)
 	List<Image> mNonFollowImage(int loginUserId);
 	
 	@Query(value="select * from Image where userId in (select toUserId from Follow where fromUserId = ?1) ORDER BY createDate DESC", nativeQuery = true)
