443993ce0280d48c8fe5a7d0b48811f5907ddf57
cocoblue <cocoblue@kakao.com>
2021-11-01 15:27:50 +0900

    Junit 5 대응

--- src/test/java/com/cos/iter/domain/post/PostTest.java
+++ src/test/java/com/cos/iter/domain/post/PostTest.java
@@ -1,23 +1,17 @@
 package com.cos.iter.domain.post;
 
 import lombok.extern.log4j.Log4j2;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import java.util.List;
-import java.util.Optional;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Log4j2
-public class PostTests {
+public class PostTest {
     @Autowired
     PostRepository postRepository;
 
