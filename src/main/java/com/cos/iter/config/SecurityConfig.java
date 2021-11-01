acca191f9075fb9ce5dad64b1322dd9d0eea0224
cocoblue <cocoblue@kakao.com>
2021-11-01 15:51:57 +0900

    - PREFIX 삭제 - Github Action 보강

--- src/main/java/com/cos/iter/config/SecurityConfig.java
+++ src/main/java/com/cos/iter/config/SecurityConfig.java
@@ -31,36 +31,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Value("${server.servlet.context-path}")
-	private String prefix;
+	//	@Value("${server.servlet.context-path}")
+	private final String prefix = "";
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("prefix: " + prefix);
 		http.csrf().disable();
 		http.authorizeRequests()
-		.antMatchers("/", "/user/**", "/follow/**", "/image/**").authenticated()
-		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.antMatchers("/", "/user/**", "/follow/**", "/image/**").authenticated()
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 //		.anyRequest().permitAll()
-		.and()
-		.formLogin()
-		.loginPage("/auth/loginForm")
-		.loginProcessingUrl("/auth/login")
-		.defaultSuccessUrl(prefix + "/")
-		.failureHandler(new AuthenticationFailureHandler() {		
-			@Override 
-			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-					AuthenticationException exception) throws IOException, ServletException {
-				response.setContentType("text/html; charset=utf-8"); 
-				PrintWriter out = response.getWriter();
-				out.print(Script.back("유저네임 혹은 비밀번호를 찾을 수 없습니다."));
-				return;
-			}
-		})
-		.and()
-		.logout()
-		.logoutUrl("/auth/logout")
-		.logoutSuccessUrl("/");
+				.and()
+				.formLogin()
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/login")
+				.defaultSuccessUrl(prefix + "/")
+				.failureHandler(new AuthenticationFailureHandler() {
+					@Override
+					public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+														AuthenticationException exception) throws IOException, ServletException {
+						response.setContentType("text/html; charset=utf-8");
+						PrintWriter out = response.getWriter();
+						out.print(Script.back("유저네임 혹은 비밀번호를 찾을 수 없습니다."));
+						return;
+					}
+				})
+				.and()
+				.logout()
+				.logoutUrl("/auth/logout")
+				.logoutSuccessUrl("/");
 	}
 }
 
