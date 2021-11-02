53049d094681722ace1d112639e19eaf7cda7ba1
cocoblue <cocoblue@kakao.com>
2021-11-02 16:48:19 +0900

    #10 - 구현 중

--- src/test/java/com/cos/iter/web/PostControllerTest.java
+++ src/test/java/com/cos/iter/web/PostControllerTest.java
@@ -1,9 +1,6 @@
 package com.cos.iter.web;
 
-import com.cos.iter.domain.user.UserRepository;
-import com.cos.iter.web.dto.JoinReqDto;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -21,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Log4j2
@@ -33,17 +34,17 @@ class PostControllerTest {
     @Autowired
     private WebApplicationContext context;
 
-    @BeforeAll
+    @BeforeTransaction
     public void saveMockData() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
 
-        final String username = "test1234";
+        final String username = "cocotest12344";
         final String password = "user6949";
-        final String email = "user@gmail.com";
-        final String name = "testusername";
+        final String email = "coco@blue.com";
+        final String name = "cocotest";
 
         String url = "http://localhost:" + port + "/auth/join";
 
@@ -78,7 +79,7 @@ class PostControllerTest {
     }
 
     @Test
-    @WithUserDetails("test1234")
+    @WithMockUser
     public void feedTestWithUser() throws Exception {
         mockMvc.perform(get("/")).andExpect(status().is2xxSuccessful());
     }
