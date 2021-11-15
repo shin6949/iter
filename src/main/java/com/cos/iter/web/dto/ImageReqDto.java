4bc67c2859b1f0d35c97f815d78b1df36b16ba9b
cocoblue <cocoblue@kakao.com>
2021-11-15 14:01:52 +0900

    ADD Feature - 이미지 다수 업로드 및 위치 다수 지정 기능 구현 완료
    Improvement
    - Feed 내 위치 제거 (API Call 횟수 줄이기)
    - 기존 image-upload.mustache 삭제

--- src/main/java/com/cos/iter/web/dto/ImageReqDto.java
+++ src/main/java/com/cos/iter/web/dto/ImageReqDto.java
@@ -2,11 +2,8 @@ package com.cos.iter.web.dto;
 
 import com.cos.iter.domain.post.Post;
 
-import com.cos.iter.domain.image.Image;
-
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.util.List;
 
@@ -14,29 +11,15 @@ import java.util.List;
 public class ImageReqDto {
 	private List<MultipartFile> file;
 	private String content;
-	private float latitude;
-	private float longitude;
-	private String tags;
-	
-	public Image toImageEntity(String imageUrl, Post postEntity, short sequence) {
-		return Image.builder()
-				.latitude(latitude)
-				.longitude(longitude)
-				.url(imageUrl)
-				.post(postEntity)
-				.sequence(sequence)
-				.build();
-	}
+	private List<Float> latitude;
+	private List<Float> longitude;
+	private List<String> locationName;
 
 	public Post toPostEntity() {
 		return Post.builder()
 				.content(content)
 				.build();
 	}
-
-//	public List<MultipartFile> getMultipartFiles() {
-//		return file.getFiles("file");
-//	}
 }
 
 
